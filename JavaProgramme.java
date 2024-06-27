d43980ce6ec02de8277f94b23b3685b3aafa142f
Md Tarek Miah <140520331+Tarek00T@users.noreply.github.com>
2024-06-27 22:23:59 +0600

    Java programme in my bio data

--- JavaProgramme.java
+++ JavaProgramme.java
@@ -1,12 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Tc
- */
-public class JavaProgramme {
-    
+package com.mycompany.javaproggm;
+
+
+public class JavaProggm {
+
+    public static void main(String[] args) {
+         System.out.println("Name : Md Tarek Miah");
+        System.out.println("ID : 231-115-208");
+         System.out.println("Section : F ");
+          System.out.println("House : Habiganj(olipur)");
+           System.out.println("Age : 23 years old");
+            System.out.println("Marrit status : unmarrite");
+             System.out.println("I love programming java language");
+              System.out.println("My favourite language is : c,c++,java,python,c#,php");
+               System.out.println("My favourite cp judge :leetcode, codeforces,codechef,atcoder,hacker earth,hacker rank");
+    }
 }
